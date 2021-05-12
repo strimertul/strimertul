ecb7a63fb75490c5c91e204dbc6fdc27fe053df5
Ash Keel <ashlordkelly@bulge.exposed>
2021-05-12 19:18:43 +0200

    Use Kilovolt NPM library for v3

--- frontend/src/lib/react-utils.ts
+++ frontend/src/lib/react-utils.ts
@@ -1,9 +1,9 @@
 import { ActionCreatorWithOptionalPayload, AsyncThunk } from '@reduxjs/toolkit';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { KilovoltMessage } from '@strimertul/kilovolt-client';
 import { RootState } from '../store';
 import { APIState } from '../store/api/reducer';
-import { wsMessage } from './strimertul-ws';
 
 export function useModule<T>({
   key,
@@ -17,10 +17,10 @@ export function useModule<T>({
   // eslint-disable-next-line @typescript-eslint/ban-types
   getter: AsyncThunk<T, void, {}>;
   // eslint-disable-next-line @typescript-eslint/ban-types
-  setter: AsyncThunk<wsMessage, T, {}>;
+  setter: AsyncThunk<KilovoltMessage, T, {}>;
   asyncSetter: ActionCreatorWithOptionalPayload<T, string>;
   // eslint-disable-next-line @typescript-eslint/ban-types
-}): [T, AsyncThunk<wsMessage, T, {}>] {
+}): [T, AsyncThunk<KilovoltMessage, T, {}>] {
   const client = useSelector((state: RootState) => state.api.client);
   const data = useSelector((state: RootState) => selector(state.api));
   const dispatch = useDispatch();
