f4a30dddb59fe7c96b03463bdec1d7601eb58814
Ash Keel <ash@nebula.cafe>
2021-11-05 18:43:33 +0100

    Add horizontal support to Field

--- frontend/src/ui/components/Field.tsx
+++ frontend/src/ui/components/Field.tsx
@@ -2,12 +2,30 @@ import React from 'react';
 
 export interface FieldProps {
   name?: string;
+  className?: string;
+  horizontal?: boolean;
 }
 
-function Field({ name, children }: React.PropsWithChildren<FieldProps>) {
+function Field({
+  name,
+  className,
+  horizontal,
+  children,
+}: React.PropsWithChildren<FieldProps>) {
+  let classes = className ?? '';
+  if (horizontal) {
+    classes += ' is-horizontal';
+  }
+  let nameEl = null;
+  if (name) {
+    nameEl = <label className="label">{name}</label>;
+    if (horizontal) {
+      nameEl = <div className="field-label is-normal">{nameEl}</div>;
+    }
+  }
   return (
-    <div className="field">
-      {name ? <label className="label">{name}</label> : null}
+    <div className={`field ${classes}`}>
+      {nameEl}
       {children}
     </div>
   );
