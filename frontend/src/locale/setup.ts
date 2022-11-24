8b911ab553468db5e4a80b29b23e8b9b757c7245
Ash Keel <ash@nebula.cafe>
2022-11-24 19:49:42 +0100

    feat: start work on italian translation

--- frontend/src/locale/setup.ts
+++ frontend/src/locale/setup.ts
@@ -2,11 +2,15 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import en from './en/translation.json';
+import it from './it/translation.json';
 
 void i18n.use(initReactI18next).init({
   resources: {
     en: {
       translation: en,
+    },
+    it: {
+      translation: it,
     },
   },
   lng: navigator.language,
