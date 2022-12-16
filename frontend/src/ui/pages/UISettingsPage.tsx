bf8cd57fff60b5888696b770c6726a0f63ea74b2
Ash Keel <ash@nebula.cafe>
2022-12-16 12:19:05 +0100

    feat: add button to repeat onboarding

--- frontend/src/ui/pages/UISettingsPage.tsx
+++ frontend/src/ui/pages/UISettingsPage.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useModule } from '~/lib/react-utils';
 import { languages } from '~/locale/languages';
+import { useAppDispatch } from '~/store';
 import { modules } from '~/store/api/reducer';
 import RadioGroup from '../components/forms/RadioGroup';
-import { Field, Label, PageContainer, PageHeader, PageTitle } from '../theme';
+import {
+  Button,
+  Field,
+  Label,
+  PageContainer,
+  PageHeader,
+  PageTitle,
+} from '../theme';
 
 export default function UISettingsPage(): React.ReactElement {
   const [uiConfig, setUiConfig] = useModule(modules.uiConfig);
   const [t, i18n] = useTranslation();
+  const dispatch = useAppDispatch();
 
   const maxKeys = languages.reduce(
     (current, it) => Math.max(current, it.keys),
@@ -33,7 +42,7 @@ export default function UISettingsPage(): React.ReactElement {
                 {lang.name}{' '}
                 {lang.keys < maxKeys ? (
                   <small>
-                    Partial translation (
+                    {t('pages.uiconfig.partial-translation')} (
                     {((lang.keys / maxKeys) * 100).toFixed(1)}% - {lang.keys}/
                     {maxKeys})
                   </small>
@@ -43,6 +52,14 @@ export default function UISettingsPage(): React.ReactElement {
           }))}
         />
       </Field>
+      <Button
+        type="button"
+        onClick={() => {
+          void dispatch(setUiConfig({ ...uiConfig, onboardingDone: false }));
+        }}
+      >
+        {t('pages.uiconfig.repeat-onboarding')}
+      </Button>
     </PageContainer>
   );
 }
