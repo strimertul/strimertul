19dc37711d996efe6fbe8041e1c1d41be24c4a8d
Ash Keel <ash@nebula.cafe>
2023-03-09 12:42:11 +0100

    fix: don't call close on null instances

--- utils/deferlogger.go
+++ utils/deferlogger.go
@@ -9,6 +9,9 @@ import (
 )
 
 func Close(res io.Closer, logger *zap.Logger) {
+	if res == nil {
+		return
+	}
 	err := res.Close()
 	if err != nil {
 		logger.Error("could not close resource", zap.String("name", reflect.TypeOf(res).String()), zap.Error(err), zap.String("stack", string(debug.Stack())))
